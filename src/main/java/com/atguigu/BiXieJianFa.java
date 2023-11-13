eb7562e3e94c0dbdd180fbaf1110ddfb30743e6c
yuebuqun <yuebuqun3333@sina.com>
2023-11-13 20:21:34 +0800

    创建辟邪剑法

---
+++ src/main/java/com/atguigu/BiXieJianFa.java
@@ -0,0 +1,16 @@
+package com.atguigu;
+
+/**
+ * ClassName: ${NAME}
+ * Description:
+ *
+ * @Author Jianlin LI
+ * @Create 2023/11/13 14:26
+ * @Version 1.0
+ */
+public class BiXieJianFa {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
