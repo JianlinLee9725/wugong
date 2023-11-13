44af6051da3eba5b2a7d88c9a3206af9355086d9
JianlinLee <1056898886@qq.com>
2023-11-13 20:29:39 +0800

    快速输出辟邪减法

--- src/main/java/com/atguigu/BiXieJianFa.java
+++ src/main/java/com/atguigu/BiXieJianFa.java
@@ -11,6 +11,14 @@ package com.atguigu;
 public class BiXieJianFa {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
+        System.out.println("辟邪剑法");
     }
 }
