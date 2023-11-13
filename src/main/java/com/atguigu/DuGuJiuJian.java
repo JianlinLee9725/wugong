a94c4fc8ce6a991c06877c515856f94ceee334c5
JianlinLee <1056898886@qq.com>
2023-11-13 20:41:06 +0800

    创建独孤九剑

---
+++ src/main/java/com/atguigu/DuGuJiuJian.java
@@ -0,0 +1,21 @@
+package com.atguigu;
+
+/**
+ * ClassName: DuGuJiuJian
+ * Description:
+ *
+ * @Author Jianlin LI
+ * @Create 2023/11/13 20:38
+ * @Version 1.0
+ */
+public class DuGuJiuJian {
+    public static void main(String[] args) {
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+    }
+}
