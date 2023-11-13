3020b109ebbbbe11a02cb8a5538f782c9db5fffa
JianlinLee9725 <1056898886@qq.com>
2023-11-13 20:55:10 +0800

    add AAAAA

--- src/main/java/com/atguigu/BiXieJianFa.java
+++ src/main/java/com/atguigu/BiXieJianFa.java
@@ -1,13 +1,6 @@
 package com.atguigu;
 
-/**
- * ClassName: ${NAME}
- * Description:
- *
- * @Author Jianlin LI
- * @Create 2023/11/13 14:26
- * @Version 1.0
- */
+
 public class BiXieJianFa {
     public static void main(String[] args) {
 
