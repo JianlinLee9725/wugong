91dfedf3e2a70585673f51ff1d39a4e6cd6d06bb
lijianlin <1056898886@qq.com>
2023-11-13 21:27:29 +0800

    TestSSH

---
+++ src/main/java/com/atguigu/TestSSH.java
@@ -0,0 +1,18 @@
+package com.atguigu;/*
+ * ClassName: TestSSH
+ * Description:
+ * @Author Jianlin LI
+ * @Create 2023/11/13 21:26
+ * @Version 1.0
+ */
+
+public class TestSSH {
+    public static void main(String[] args) {
+        System.out.println("testSSH");
+        System.out.println("testSSH");
+        System.out.println("testSSH");
+        System.out.println("testSSH");
+        System.out.println("testSSH");
+        System.out.println("testSSH");
+    }
+}
