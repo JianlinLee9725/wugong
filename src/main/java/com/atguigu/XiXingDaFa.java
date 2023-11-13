10696c9ab692ea325adc9c234a3cc749830d9cca
JianlinLee9725 <1056898886@qq.com>
2023-11-13 21:08:10 +0800

    add BBBBBBB

---
+++ src/main/java/com/atguigu/XiXingDaFa.java
@@ -0,0 +1,19 @@
+package com.atguigu;/*
+ * ClassName: XiXingDaFa
+ * Description:
+ * @Author Jianlin LI
+ * @Create 2023/11/13 21:07
+ * @Version 1.0
+ */
+
+public class XiXingDaFa {
+    public static void main(String[] args) {
+        System.out.println("吸星大法");
+        System.out.println("吸星大法");
+        System.out.println("吸星大法");
+        System.out.println("吸星大法");
+        System.out.println("吸星大法");
+        System.out.println("吸星大法");
+        System.out.println("吸星大法");
+    }
+}
